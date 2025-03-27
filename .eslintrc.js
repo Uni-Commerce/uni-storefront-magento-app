97a8b1bb79d3e059f37f48b9c91f536857922a30
caesar <454451758@qq.com>
2025-03-27 18:23:03 +0800

    chore: babel resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  rules: {
+    curly: 0,
+    semi: 0,
+    'comma-dangle': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/no-unstable-nested-components': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react-native/no-inline-styles': 0
+  }
+}
---
+++ src/components/AppLayout/appLayout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const AppLayout = () => {
+  return (
+    <View>
+      <Text>App Layout</Text>
+    </View>
+  )
+}
+
+export default AppLayout
